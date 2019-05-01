91387252d3dca7636a293e3588f596a9afe02d19
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-04-30 18:48:06 -0700

    while, for in, for of loops

--- js/loops.js
+++ js/loops.js
@@ -1,5 +1,6 @@
 var fruits = ['apple', 'orange', 'blueberry', 'pineapple', 'kiwi', 'strawberry'];
 console.log(fruits);
+
 // console.log(fruits[0]);
 // console.log(fruits[1]);
 // console.log(fruits[2]);
@@ -15,7 +16,25 @@ console.log(fruits);
 
 //do while
 var index = 0;
-do {
-    console.log(fruits[index]);
-    index++;
-} while (index < fruits.length);
+// do {
+//     console.log(fruits[index]);
+//     index++;
+// } while (index < fruits.length);
+
+//while
+// while (index < fruits.length) {
+//     console.log(fruits[index]);
+//     // index++;
+//     index += 1;
+// }
+
+//for in
+fruits.foo = 'test';
+for (var i in fruits) {
+    console.log(i);
+}
+
+//for of
+for (var i of fruits) {
+    console.log(i);
+}
